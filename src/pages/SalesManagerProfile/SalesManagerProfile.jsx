e57ee9d96cc0f4cce249ea761e7ec0ef27961995
Suryakrishnavp <suryakrishnavaliyapurakkal@gmail.com>
2025-04-17 13:58:38 +0530

    Fix Photo Error

--- src/pages/SalesManagerProfile/SalesManagerProfile.jsx
+++ src/pages/SalesManagerProfile/SalesManagerProfile.jsx
@@ -40,9 +40,9 @@ const SalesManagerProfile = () => {
       <div className={styles.profileCard}>
         <div className={styles.imageSection}>
         <img
-            src={profile.photo ? `https://devlokcrm-production.up.railway.app${profile.photo}` : ProfileImage}
+            src={profile.photo ? `https://devlokcrm-production.up.railway.app/${profile.photo}` : ProfileImage}
             alt={profile.username}
-            className={styles.profileImage}
+            className={styles.profileImg}
             onError={(e) => {
               e.target.onerror = null;
               e.target.src = ProfileImage;
