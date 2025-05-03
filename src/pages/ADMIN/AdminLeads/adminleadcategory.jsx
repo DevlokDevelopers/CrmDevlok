99faaed76ea27eb2949f600603ebc82ac50c268e
Suryakrishnavp <suryakrishnavaliyapurakkal@gmail.com>
2025-05-03 21:52:49 +0530

    Lead category

--- src/pages/ADMIN/AdminLeads/adminleadcategory.jsx
+++ src/pages/ADMIN/AdminLeads/adminleadcategory.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
-import styles from "./leadcategory.module.css";
+import styles from "./AdminLeads.module.css";
 import AdminLayout from "../../../components/Layouts/AdminLayout";
 import { Bar } from "react-chartjs-2";
 import {
