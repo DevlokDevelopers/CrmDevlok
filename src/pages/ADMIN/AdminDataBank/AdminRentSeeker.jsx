87f23f3b798c5b334aeae97ae71eecf447fc3fa9
DevlokDevelopers <devlokpromotions@gmail.com>
2025-05-09 11:30:27 +0530

    Speed and Performance

--- src/pages/ADMIN/AdminDataBank/AdminRentSeeker.jsx
+++ src/pages/ADMIN/AdminDataBank/AdminRentSeeker.jsx
@@ -6,7 +6,7 @@ import AdminLayout from "../../../components/Layouts/AdminLayout";
 import UploadImageModal from "../../../components/Modals/AddImageModal";
 import FilterModal from "../../../components/Modals/FilterModal";
 import filterIcon from "../../../assets/setting-4.svg";
-
+import FancySpinner from "../../../components/Loader/Loader";
 const AdminRentseekerList = () => {
   const [data, setData] = useState([]);
   const [error, setError] = useState("");
@@ -17,6 +17,8 @@ const AdminRentseekerList = () => {
   const itemsPerPage = 8;
   const navigate = useNavigate();
   const location = useLocation();
+  const [loading, setLoading] = useState(false);
+
 
   const tabPaths = {
     Analytics: "/admin_data_graph",
@@ -43,6 +45,7 @@ const AdminRentseekerList = () => {
       navigate("/login");
       return;
     }
+    setLoading(true);
 
     try {
       const response = await axios.get("https://devlokcrmbackend.up.railway.app/databank/rental_seeker/", {
@@ -54,6 +57,9 @@ const AdminRentseekerList = () => {
     } catch (error) {
       console.error("Error fetching data:", error);
       setError("Failed to fetch data. Try again later.");
+    }
+    finally {
+      setLoading(false); // Hide spinner
     }
   };
 
@@ -122,11 +128,15 @@ const AdminRentseekerList = () => {
         )}
 
         {/* Content */}
-        {error ? (
-          <p className={styles.error}>{error}</p>
-        ) : data.length === 0 ? (
-          <p className={styles.noData}>No data available.</p>
-        ) : (
+        {loading ? (
+  <div className={styles.loaderWrapper}>
+    <FancySpinner />
+  </div>
+) : error ? (
+  <p className={styles.error}>{error}</p>
+) : data.length === 0 ? (
+  <p className={styles.noData}>No data available.</p>
+) : (
           <div className={styles.leadContainer}>
             {currentItems.map((item) => (
               <div key={item.id} className={styles.leadCard}>
