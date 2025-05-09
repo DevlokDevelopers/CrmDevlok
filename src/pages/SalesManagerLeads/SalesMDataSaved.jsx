47b7a88b256a8ff431982137d6869c989374c71e
DevlokDevelopers <devlokpromotions@gmail.com>
2025-05-09 19:26:44 +0530

    Speed and Performance

--- src/pages/SalesManagerLeads/SalesMDataSaved.jsx
+++ src/pages/SalesManagerLeads/SalesMDataSaved.jsx
@@ -214,12 +214,19 @@ const DataSavedLeads = () => {
                     
                     <div className={styles.infoBlock}>
                     <button
-                    className={`${styles.followUpBtn} ${buttonLoadingId === lead.id ? styles.spinner : ""}`}
-                    onClick={() => handleViewData(lead)}
-                    disabled={buttonLoadingId === lead.id}
-                  >
-                    {buttonLoadingId === lead.id ? "Fetching..." : "View Data"}
-                  </button>
+  className={styles.followUpBtn}
+  onClick={() => handleViewData(lead)}
+  disabled={buttonLoadingId === lead.id}
+>
+  {buttonLoadingId === lead.id ? (
+    <>
+      Fetching... <span className={styles.spinner}></span>
+    </>
+  ) : (
+    "View Data"
+  )}
+</button>
+
 
 
 
