12a2634e28a7b94e5780e4c0ff42e0438f84bcda
DevlokDevelopers <devlokpromotions@gmail.com>
2025-05-10 20:21:12 +0530

    Speed and Performance

--- src/pages/ADMIN/AdminProject/LoadingScreen.jsx
+++ src/pages/ADMIN/AdminProject/LoadingScreen.jsx
@@ -1,16 +1,15 @@
-// components/LoadingScreen.js
 import React from "react";
 import "./LoadingScreen.css";
 
 const LoadingScreen = () => {
   return (
     <div className="fullscreen-loader">
-      <div className="leaves-container">
-        <div className="leaf leaf1"></div>
-        <div className="leaf leaf2"></div>
-        <div className="leaf leaf3"></div>
-        <div className="leaf leaf4"></div>
-        <div className="leaf leaf5"></div>
+      <div className="flowers-container">
+        <div className="flower flower1"></div>
+        <div className="flower flower2"></div>
+        <div className="flower flower3"></div>
+        <div className="flower flower4"></div>
+        <div className="flower flower5"></div>
       </div>
       <p className="loading-text">Loading your project...</p>
     </div>
