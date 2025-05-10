045db9ff9984039a8b3cfad9e7071ebe516d37c3
DevlokDevelopers <devlokpromotions@gmail.com>
2025-05-10 20:11:42 +0530

    Speed and Performance

--- src/pages/ADMIN/AdminProject/SingleAdminProject.jsx
+++ src/pages/ADMIN/AdminProject/SingleAdminProject.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
 import AdminLayout from "../../../components/Layouts/AdminLayout";
 import styles from "./SingleAdminProject.module.css";
-import LoadingScreen from "../../../components/LoadingScreen";
+import LoadingScreen from "./LoadingScreen";
 
 
 const AdminProjectDetails = () => {
