c1ce3f7c9967e30953bb175083d8deb6f5792525
DevlokDevelopers <devlokpromotions@gmail.com>
2025-05-10 20:25:47 +0530

    Speed and Performance

--- src/pages/ADMIN/AdminProject/LoadingScreen.jsx
+++ src/pages/ADMIN/AdminProject/LoadingScreen.jsx
@@ -3,17 +3,17 @@ import "./LoadingScreen.css";
 
 const LoadingScreen = () => {
   return (
-    <div className="fullscreen-loader">
-      <div className="flowers-container">
-        <div className="flower flower1"></div>
-        <div className="flower flower2"></div>
-        <div className="flower flower3"></div>
-        <div className="flower flower4"></div>
-        <div className="flower flower5"></div>
-      </div>
-      <p className="loading-text">Loading your project...</p>
-    </div>
-  );
+    <div class="wave-wrapper">
+        <div class="wave-container">
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+        </div>
+        <div class="wave-text">Loading Project...</div>
+        </div>
+
+        );
 };
 
 export default LoadingScreen;
