2ce5ba76ac6e2dedf402c55d6c3191fa7460285f
DevlokDevelopers <devlokpromotions@gmail.com>
2025-05-10 20:14:36 +0530

    Speed and Performance

--- src/pages/ADMIN/AdminProject/LoadingScreen.jsx
+++ src/pages/ADMIN/AdminProject/LoadingScreen.jsx
@@ -1,17 +1,17 @@
 // components/LoadingScreen.js
 import React from "react";
-import { Player } from "@lottiefiles/react-lottie-player";
 import "./LoadingScreen.css";
 
 const LoadingScreen = () => {
   return (
     <div className="fullscreen-loader">
-      <Player
-        autoplay
-        loop
-        src="https://assets9.lottiefiles.com/packages/lf20_s1eypznm.json" // Deer running animation
-        style={{ height: "300px", width: "300px" }}
-      />
+      <div className="leaves-container">
+        <div className="leaf leaf1"></div>
+        <div className="leaf leaf2"></div>
+        <div className="leaf leaf3"></div>
+        <div className="leaf leaf4"></div>
+        <div className="leaf leaf5"></div>
+      </div>
       <p className="loading-text">Loading your project...</p>
     </div>
   );
